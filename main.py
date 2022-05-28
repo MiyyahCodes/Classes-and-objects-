ae491ee60aec93cd97a7b5f924b44c858d9e3a75
Adebimpe Islamiyyah Adefila <95176308+MiyyahCodes@users.noreply.github.com>
2022-05-28 13:22:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,34 @@
-class Student: 
+class Student:
     # [assignment] Skeleton class. Add your code here
-    def __init__(self, name, age, tracks, score):
-        self.name = str(name)
-        self.age = int(age)
-        self.tracks = tracks
-        self.score = float(score)
+    def __init__(miyyah, name, age, tracks, score):
+        miyyah.name = str(name)        
+        miyyah.age = int(age)
+        miyyah.tracks = str(tracks)
+        miyyah.score = float(score)
         
-    def change_name(self, change_name):
-        self.change_name = change_name
-        print ("Your name is" , change_name)
-    
-    def change_age(self, change_age):
-        self.change_age = change_age
-        print ("Your age is" , change_age)
-    
-    def add_tracks(self, add_track):
-        self.add_track = add_track
-        print ("Your track is", add_track)
-    
-    def get_score(self):
-        return ("Your score is", self.score)
+    def change_name(miyyah, change_name):
+            miyyah.change_name = change_name
+            print("Thy name is", change_name)
+        
+    def change_age(miyyah, change_age):
+            miyyah.change_age = change_age
+            print(miyyah.change_name + " is ", change_age , " years old.")
 
-Bob = Student(name="Bob", age=26, tracks=["FE","BE"], score=20.90)
+    def add_track(miyyah, add_track):
+            miyyah.add_track = add_track
+            print("Track of", miyyah.change_name ," is ", add_track)
+        
+    def get_score(miyyah):
+            print(miyyah.change_name, " score is ", miyyah.score)
+        
+    pass
+
+
+
+Bob = Student(name="Bob", age=26, tracks=["FE","BE"],score=20.90)
 
 # Expected methods
 Bob.change_name("Peter")
 Bob.change_age(34)
-Bob.add_tracks("Full stack")
+Bob.add_track("UI/UX")
 Bob.get_score()
